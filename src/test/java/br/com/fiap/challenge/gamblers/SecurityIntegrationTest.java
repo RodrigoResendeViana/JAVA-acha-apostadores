f70b0bf035ae6ddfa6de30e670c1d2eedf7c07fa
Rodrigo <digo1209@gmail.com>
2025-10-20 22:01:06 -0300

    fix: pipeline

--- src/test/java/br/com/fiap/challenge/gamblers/SecurityIntegrationTest.java
+++ src/test/java/br/com/fiap/challenge/gamblers/SecurityIntegrationTest.java
@@ -3,7 +3,7 @@ package br.com.fiap.challenge.gamblers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.HttpEntity;
